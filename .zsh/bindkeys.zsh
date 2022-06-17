575989c0a9a4c23bdae861a3b115aed0984d69fc
alexmalder <alexmalder@mail.ru>
2022-06-17 18:06:14 +0300

    rm.zsh

--- .zsh/bindkeys.zsh
+++
@@ -1,8 +0,0 @@
-# BIND KEYS
-bindkey '^x' backward-delete-char
-bindkey '^a' beginning-of-line
-bindkey '^e' end-of-line
-bindkey '^]' forward-word
-bindkey '^[' backward-word
-#bindkey '^n' history-substring-search-up
-#bindkey '^p' history-substring-search-down
